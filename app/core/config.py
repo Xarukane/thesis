06606d2b5d2bf93bf193fa4a20c38ebc1be9f8bb
Xarukane <sarvarsherov6884@gmail.com>
2026-04-30 17:41:44 +0200

    Version 3.2.1

--- app/core/config.py
+++ app/core/config.py
@@ -38,7 +38,7 @@ class Settings(BaseSettings):
 
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
-    BACKEND_CORS_ORIGINS: list[str] = [
+    BACKEND_CORS_ORIGINS: Any = [
         "http://localhost:3000",
         "http://localhost:5173",
         "http://localhost:8000",
